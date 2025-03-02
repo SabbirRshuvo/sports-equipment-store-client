8904a269cc89df3056cf94ce4fe00b81bb63c7b6
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-03-03 04:52:10 +0600

    something is changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,3 @@
-/* eslint-disable no-unused-vars */
-// Sample animation file
-import { motion } from "framer-motion";
-import { div } from "framer-motion/client";
-import { Fade } from "react-awesome-reveal";
-import { Link } from "react-router";
-
 const Home = () => {
   return (
     <div>
